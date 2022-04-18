d526716bc3e6c695a3aaf59136dd8ec2a4d6afec
Дмитрий Козырев <dima02ufa@yandex.ru>
2022-04-18 11:25:00 +0500

    Рабочий тест для создания контака в записной книге (как в задании 2). Читаемость ещё не повышена.

--- addressbook-web-tests/addressbook-web-tests/Square.cs
+++ addressbook-web-tests/addressbook-web-tests/Square.cs
@@ -9,9 +9,8 @@ namespace addressbook_web_tests
     class Square : Figure
     {
         private int size;
-        private bool colored = false;
-
-        public Square(int size)
+        
+                public Square(int size)
         {
             this.size = size;
         }
